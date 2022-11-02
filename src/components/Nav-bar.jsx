d4bf2f1c9af5788e1bdbb8d9b063efe139490c33
Stas Fastovets <fastovets.stas@gmail.com>
2022-11-02 13:46:31 +0200

    Update Nav-bar.jsx

--- src/components/Nav-bar.jsx
+++ src/components/Nav-bar.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
             <li className="nav-bar__item"><a className="nav-bar__link" href="#">Messages</a></li>
             <li className="nav-bar__item"><a className="nav-bar__link" href="#">News</a></li>
             <li className="nav-bar__item"><a className="nav-bar__link" href="#">Music</a></li>
-            <li className="nav-bar__item"><a className="nav-bar__link" href="#">Settings</a></li>
+            {/* <li className="nav-bar__item"><a className="nav-bar__link" href="#">Settings</a></li> */}
          </ul>
       </nav>
    )
