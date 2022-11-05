91ca5b5889b36f9b1bd1b0cf64fde1f8655542f5
Stas Fastovets <fastovets.stas@gmail.com>
2022-11-05 10:38:22 +0200

    Application of props in practice

--- src/components/profile/posts/Posts.jsx
+++ src/components/profile/posts/Posts.jsx
@@ -9,10 +9,10 @@ const Posts = () => {
             <textarea className='posts__form-area' name="comments"></textarea>
             <button className='posts__form-button' type='submit'>Send</button>
          </form>
-         <Post />
-         <Post />
-         <Post />
-         <Post />
+         <Post message="Hi, how are you?" likes="5" />
+         <Post message="I want to see you?" likes="3" />
+         <Post message="How old are you?" likes="2" />
+         <Post message="You are great person :)" likes="1" />
       </div>
    )
 }
--- src/components/profile/posts/one_post/One_post.jsx
+++ src/components/profile/posts/one_post/One_post.jsx
@@ -1,10 +1,16 @@
 import './One_post.css'
 
-const Post = () => {
+const Post = (props) => {
    return (
       <div className="post">
-         <div className="post__img"><img className="post__img_yellow" src="https://img.freepik.com/free-vector/yellow-background-with-halftone-lines-design_1017-30148.jpg?w=360" alt="img_yellow" /></div>
-         <div className="post__text">Hello!</div>
+         <div className="post__top">
+            <div className="post__img"><img className="post__img_yellow" src="https://img.freepik.com/free-vector/yellow-background-with-halftone-lines-design_1017-30148.jpg?w=360" alt="img_yellow" /></div>
+            <div className="post__text">{props.message}</div>
+         </div>
+         <div className="post__bottom">
+            <div className="post__likes">likes</div>
+            <div className="post__likes-count">{props.likes}</div>
+         </div>
       </div>
    )
 }
